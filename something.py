d90495053bb0f0d245191c30abfd6a38833ea3dd
The-Ancient-One-1 <75278602+The-Ancient-One-1@users.noreply.github.com>
2021-01-10 21:28:52 +0200

    printing after branching

--- something.py
+++ something.py
@@ -3,3 +3,4 @@ print("hello world of github")
 for i in range(5):
   print("hello there")
 # adding a comment to explain things
+print("this is the main trunk of the repository, and the way things change here dosen't change the way things are going in the pre-dfined branches")
